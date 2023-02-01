21ef9aa9d8dd7e09783365b9c12a46914fd2637c
binh.nd <binhk32xp@gmail.com>
2023-02-01 22:20:13 +0700

    update login page

--- src/features/app/apis/appApis.ts
+++ src/features/app/apis/appApis.ts
@@ -1,18 +1,18 @@
-import { apis } from 'apis/apis'
-import { LoginRequest } from './models/login/request'
-import { LoginResponse } from './models/login/response'
+import { apis } from "apis/apis";
+import { LoginRequest } from "./models/login/request";
+import { LoginResponse } from "./models/login/response";
 
 const authApis = apis.injectEndpoints({
-  endpoints: (builder) => ({
-    login: builder.mutation<LoginResponse, LoginRequest>({
-      query: (params) => ({
-        url: 'admin/authenticate',
-        method: 'POST',
-        body: params,
-      }),
-    }),
-  }),
-  overrideExisting: false,
-})
+	endpoints: (builder) => ({
+		login: builder.mutation<LoginResponse, LoginRequest>({
+			query: (params) => ({
+				url: "auth/login",
+				method: "POST",
+				body: params,
+			}),
+		}),
+	}),
+	overrideExisting: false,
+});
 
-export const { useLoginMutation } = authApis
+export const { useLoginMutation } = authApis;
