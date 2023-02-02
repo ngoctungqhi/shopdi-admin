22f03d0f2561bd4514d3ddbef1f727f83bf6911d
binh.nd <>
2023-02-02 18:22:26 +0700

    Update login page

--- src/features/app/components/appLoginPage/appLoginPage.tsx
+++ src/features/app/components/appLoginPage/appLoginPage.tsx
@@ -1,7 +1,9 @@
 import { memo } from 'react'
-import { Form, Input, Button, Col, Row } from 'antd'
+import { Form, Input, Button, Col, Row, Checkbox } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import { useAppLoginPage } from './useAppLoginPage'
+import logoSvg from 'assets/logo.svg'
+import lineSvg from 'assets/line.svg'
 
 export const AppLoginPage = memo(() => {
   const { handleSubmitForm } = useAppLoginPage()
@@ -9,53 +11,65 @@ export const AppLoginPage = memo(() => {
   return (
     <>
       <div className="relative w-screen h-screen">
-        <div
-          className="absolute top-1/2 left-1/2 w-1/4 bg-blue-300 p-8 rounded-md"
-          style={{ transform: 'translate(-50%, -50%)' }}
-        >
-          <Row>
-            <Col span={24}>
-              <Form onFinish={handleSubmitForm}>
+      <Row gutter={[0, 0]} className="h-full">
+      <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+            <div className="bg-[#374756] h-full flex flex-col items-center justify-center">
+              <div className="Login-text flex flex-col items-center justify-center">
+                <img src={logoSvg} className="w-[200px] h-auto" alt="" />
+                <h2 className="font-semibold text-2xl mb-2 text-white mt-[10px]">SHOPDI ECOMMERCE ADMIN</h2>
+                <p className="text-lg text-white">Sàn thương mại điện tử thế hệ mới</p>
+              </div>
+              <div className="absolute bottom-0 left-0">
+                <img src={lineSvg} alt="" />
+              </div>
+            </div>
+          </Col>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+            <div className="pt-[250px] flex flex-col justify-center items-center">
+                <div>
+                <h1 className='mb-0 text-3xl font-semibold'>Đăng nhập</h1>
+                <h5 className='text-[rgba(0,0,0,0.45)] text-sm'>Shopdi chào mừng bạn đã quay lại!</h5>
+                <Form onFinish={handleSubmitForm} className="mt-10 max-w-sm">
                 <Form.Item
                   name="username"
                   rules={[
-                    { required: true, message: 'Please input your username!' },
+                    { required: true, message: 'Vui lòng nhập tên đăng nhập' },
                   ]}
+                  hasFeedback
                 >
                   <Input
-                    prefix={
-                      <UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
-                    }
-                    placeholder="Username"
+                    prefix={<UserOutlined />}
+                    className="h-10"
+                    placeholder="Tên đăng nhập"
                   />
                 </Form.Item>
                 <Form.Item
                   name="password"
                   rules={[
-                    { required: true, message: 'Please input your password!' },
+                    { required: true, message: 'Vui lòng nhập mật khẩu' },
                   ]}
                 >
                   <Input
-                    prefix={
-                      <LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
-                    }
+                    prefix={<LockOutlined />}
+                    className="h-10"
                     type="password"
                     placeholder="Password"
                   />
                 </Form.Item>
                 <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className="login-form-button"
-                  >
-                    Log in
-                  </Button>
-                </Form.Item>
+                    <Checkbox>Nhớ mật khẩu</Checkbox>
+                    <span className="text-[#1890ff] float-right cursor-pointer">
+                      Quên mật khẩu?
+                    </span>
+                    <Button className="w-full mt-10 bg-black text-base text-white h-10 hover:bg-[#fdc500] hover:border-none hover:outline-none" htmlType="submit">
+                      Đăng nhập
+                    </Button>
+                  </Form.Item>
               </Form>
+                </div>
+            </div>
             </Col>
           </Row>
-        </div>
       </div>
     </>
   )
